05e3169958cacf5790123a9411ad0a76493ac354
MDS\mario.adi8385 <mario.adisurya@gmail.com>
2018-08-17 09:44:23 +1200

    First initial commit / push of LibertyGame into master branch

---
+++ Assets/Scripts/CharMoveStateManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum CharMoveStates { 	CHAR_LEFT, CHAR_RIGHT, CHAR_UP, CHAR_DOWN,
+								CHAR_UP_LEFT, CHAR_UP_RIGHT, CHAR_DOWN_LEFT, CHAR_DOWN_RIGHT,
+								CHAR_IDLE }
+
+public class CharMoveStateManager : MonoBehaviour {
+
+	protected CharMoveStates currentCharMoveState = CharMoveStates.CHAR_IDLE;
+
+	private float verticalInputVal;
+	private float horizontalInputVal;
+
+	void Update()
+	{
+		// Get the input for vertical and horizontal presses
+		verticalInputVal = Input.GetAxis("Vertical");
+		horizontalInputVal = Input.GetAxis("Horizontal");
+	}
+
+	// void CheckMovementDirection()
+	// {
+
+	// }
+}
---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class CharacterMovement : MonoBehaviour {
+
+	// public CharMoveStateManager charMoveStateManager;
+	private float verticalInputVal;
+	private float horizontalInputVal;
+
+	private Rigidbody m_RigidBody;
+
+	public float speed = 2;
+
+	void Start()
+	{
+		m_RigidBody = GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+		// Get Input for vertical and horizontal presses
+		verticalInputVal = Input.GetAxis("Vertical");
+		horizontalInputVal = Input.GetAxis("Horizontal");
+
+		Move(verticalInputVal, horizontalInputVal, speed);
+	}
+
+	// Method that handles the moving of the character
+	void Move(float v, float h, float speed)
+	{
+		float moveX = v * speed;
+		float moveZ = h * speed * -1;
+
+		m_RigidBody.AddForce(moveX, 0, moveZ);
+
+		if (moveX == 0 && moveZ == 0) { m_RigidBody.velocity = Vector3.zero; }
+	}
+}
